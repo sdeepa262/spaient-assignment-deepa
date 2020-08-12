950e1f4eb491c919d20d7135391d4e57c97bd474
sdeepa262 <61728561+sdeepa262@users.noreply.github.com>
2020-08-12 13:16:16 +0530

    Add files via upload

---
+++ build/precache-manifest.62d5e7f900d0b913af3bb477787b3347.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e346d9d2d851ef1bc3e1cbdd076092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad86d57539ede00d7ea7",
+    "url": "/static/css/2.98439ec7.chunk.css"
+  },
+  {
+    "revision": "c6b91347d0468306c07f",
+    "url": "/static/css/main.38ba0028.chunk.css"
+  },
+  {
+    "revision": "ad86d57539ede00d7ea7",
+    "url": "/static/js/2.d7d650f4.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/static/js/2.d7d650f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b91347d0468306c07f",
+    "url": "/static/js/main.8b513d88.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
